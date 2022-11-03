b7bebf7b9744c0ac9d8655622520eece371bb818
supreeth10 <smuddu2@pdx.edu>
2022-11-03 13:57:38 -0700

    Code refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,30 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import "./App.css";
-import axios from "axios";
-
 import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    // Initially, no file is selected
     selectedFile: null,
   };
 
-  // On file select (from the pop up)
   onFileChange = (event) => {
-    // Update the state
     this.setState({ selectedFile: event.target.files[0] });
   };
 
-  // On file upload (click the upload button)
   onFileUpload = () => {
-    // Create an object of formData
     const formData = new FormData();
 
-    // Update the formData object
     formData.append(
       "myFile",
       this.state.selectedFile,
       this.state.selectedFile.name
     );
 
-    // Details of the uploaded file
     console.log(this.state.selectedFile);
 
-    // Request made to the backend api
-    // Send formData object
-    axios
-      .post("api/uploadfile", formData)
+    fetch("api/uploadfile", {
+      method: "POST",
+      body: formData,
+    })
       .then((response) => {
         console.log("Request successful", response);
       })
@@ -67,8 +33,6 @@ class App extends Component {
       });
   };
 
-  // File content to be displayed after
-  // file upload is complete
   fileData = () => {
     if (this.state.selectedFile) {
       return (
@@ -97,20 +61,18 @@ class App extends Component {
 
   render() {
     return (
-      <div class="main_div">
+      <div className="main_div">
         <header className="App-header">
-          <h1 class="App">Click-n-Split</h1>
+          <h1 className="App">Click-n-Split</h1>
         </header>
-        <body>
-          <main class="App body">
-            <h3>Upload the picture of the bill</h3>
-            <div>
-              <input type="file" onChange={this.onFileChange} />
-              <button onClick={this.onFileUpload}>Upload!</button>
-            </div>
-            {this.fileData()}
-          </main>
-        </body>
+        <main className="App">
+          <h3>Upload the picture of the bill</h3>
+          <div>
+            <input type="file" onChange={this.onFileChange} />
+            <button onClick={this.onFileUpload}>Upload!</button>
+          </div>
+          {this.fileData()}
+        </main>
       </div>
     );
   }
