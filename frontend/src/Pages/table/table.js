c2e293f345caa0fa1fa40385a71aca015e8339a4
Sarvésh Biradar <sarveshbiradar@Sarveshs-M1BP.local>
2022-11-17 17:04:45 -0800

    added review page

---
+++ frontend/src/Pages/table/table.js
@@ -0,0 +1,91 @@
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+
+
+
+function TablePage() {
+    const [data, setData] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [rows, setRows] = useState([])
+    const [col, setCol] = useState([])
+    useEffect(() => {
+        setData(JSON.parse( localStorage.getItem('tableData')))
+        setIsLoaded(true)
+        // fetch("https://jsonplaceholder.typicode.com/todos/1")
+            // .then(res => res.json())
+            // .then(
+            //     (result) => {
+            //         setIsLoaded(true);
+            //         localStorage.setItem('tableData', JSON.stringify(result))
+            //         setData(result);
+            //     },
+            //     (error) => {
+            //         setIsLoaded(true);
+            //         setData('error');
+            //     }
+            // )
+    }, [])
+
+
+    const getColumns = () => {
+        let temp = []
+        for (const [key, value] of Object.entries(data[0])) {
+            temp.push(key.toLocaleUpperCase())
+        }
+        return temp
+    }
+
+    const buildColumns = () => {
+        return getColumns()
+            .map(column =>
+                <th>
+                    {column}
+                </th>
+            )
+    }
+
+    const buildSingleRow = (row) => {
+        let temp = []
+        for (const [key, value] of Object.entries(row)) {
+            temp.push(<td>
+                {value}
+            </td>);
+        }
+        return temp
+    }
+
+    const buildRows = () => {
+        let temp = []
+        data.forEach((row = {}, index) => {
+            temp.push(<tr>
+                {buildSingleRow(row)}
+            </tr>)
+        })
+        return temp
+    }
+
+    const buildTable = () => {
+        return <table>
+            <tr>
+                {buildColumns()}
+            </tr>
+            {buildRows()}
+        </table>
+    }
+
+    return (
+        <div>
+            <h3>Review your purchases</h3>
+            <div>
+                {!isLoaded && <p>...Loading</p>}
+                {isLoaded && buildTable()}
+            </div>
+            <Link to={'selection'}>
+                <button>Done!</button>
+            </Link>
+        </div>
+    );
+}
+
+
+export default TablePage;
