319f350850dbf628089ec65f7da052db10d15344
Sarvésh Biradar <sarveshbiradar@Sarveshs-M1BP.local>
2022-11-17 17:07:22 -0800

    WIP added person share page

---
+++ frontend/src/Pages/contribution/contribution.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {Link, useParams} from "react-router-dom";
+
+function Contri(props) {
+    const [contriData, setContriData] = useState(false)
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos/1")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setContriData(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setContriData('error');
+                }
+            )
+    }, [])
+
+    return isLoaded ? (
+        <div>
+            <h3>Individual Contributions</h3>
+            <div>
+
+            </div>
+        </div>
+    ):(<div>
+        <p>Loading...</p>
+    </div>);
+}
+
+export default Contri;
